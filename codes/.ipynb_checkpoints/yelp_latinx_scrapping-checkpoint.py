5f4bf17f9083228e2bceeead808005400d5006f2
anjali8203 <anjali8203@gmail.com>
2023-07-10 11:17:51 -0400

    Adding 3 files

---
+++ codes/.ipynb_checkpoints/yelp_latinx_scrapping-checkpoint.py
@@ -0,0 +1,33 @@
+import csv
+import requests
+from bs4 import BeautifulSoup
+
+global url
+
+i = [0,10,20,30,40,50,60,70,80,90,100,110,120,130,140,150,160,170,180,190,200,210,220,230]
+
+filename = 'yelp_latinx_directory.csv'
+with open(filename, 'a', newline='', encoding='utf-8') as file:
+    writer = csv.writer(file)
+    writer.writerow(['Name', 'URL'])
+
+    for page in i:
+        url = 'https://www.yelp.com/search?find_desc=Latinx+Owned+Businesses&find_loc=Fairfax%2C+VA&start=' + str(page)
+
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, 'html.parser')
+
+        div_elements = soup.find('ul', {'class': 'undefined list__09f24__ynIEd'})
+
+        for div_element in div_elements:
+            name_element = div_element.find('span', {'class': 'css-1egxyvc'})
+            url_element = div_element.find('a')
+
+            if name_element is not None and url_element is not None:
+                name = name_element.get_text(strip=True)
+                url = url_element['href']
+
+                writer.writerow([name, url])
+
+print('Success!')
+
